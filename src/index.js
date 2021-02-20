3a8428151331c265601d41a54dba64427c78e920
mailys50 <mailysbrizuela@gmail.com>
2021-02-20 11:18:41 -0300

    culminacion del proyecto

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 import cipher from './cipher.js';
 // funcion para pasar de una vista a la otra("block" para que permanesca abierta y "none" para que permanesca cerrada)
-let redirectCipher = (clase) =>
-{
-  var vistas = [".iniciar", ".segundaVista",".instruccion"];
+let redirectCipher = (clase) => {
+  var vistas = [".iniciar", ".segundaVista", ".instruccion"];
   for (let i = 0; i < vistas.length; i++) {
-    if (clase==vistas[i]) {
-      document.querySelector(vistas[i]).style.display="block";
+    if (clase == vistas[i]) {
+      document.querySelector(vistas[i]).style.display = "block";
     } else {
-      document.querySelector(vistas[i]).style.display="none";
+      document.querySelector(vistas[i]).style.display = "none";
     }
   }
 
 }
-document.getElementById("botonIniciar").addEventListener("click", () => {redirectCipher(".segundaVista")});
-document.getElementById("botonInstruccion").addEventListener("click", () => {redirectCipher(".instruccion")});
-document.getElementById("botonDescifrar2").addEventListener("click", () => {redirectCipher(".segundaVista")});
-document.getElementById("botonCifrar2").addEventListener("click", () => {redirectCipher(".segundaVista")});
-document.getElementById("botonInstruccion2").addEventListener("click",()=>{redirectCipher(".instruccion")});
-document.getElementById("botonVolver").addEventListener("click",()=>{redirectCipher(".iniciar")});
-document.getElementById("botonVolver1").addEventListener("click",()=>{redirectCipher(".iniciar")});
+document.getElementById("botonIniciar").addEventListener("click", () => { redirectCipher(".segundaVista") });
+document.getElementById("botonInstruccion").addEventListener("click", () => { redirectCipher(".instruccion") });
+document.getElementById("botonDescifrar2").addEventListener("click", () => { redirectCipher(".segundaVista") });
+document.getElementById("botonCifrar2").addEventListener("click", () => { redirectCipher(".segundaVista") });
+document.getElementById("botonInstruccion2").addEventListener("click", () => { redirectCipher(".instruccion") });
+document.getElementById("botonVolver").addEventListener("click", () => { redirectCipher(".iniciar") });
+document.getElementById("botonVolver1").addEventListener("click", () => { redirectCipher(".iniciar") });
 
 
 
@@ -27,12 +26,12 @@ document.getElementById("botonVolver1").addEventListener("click",()=>{redirectCi
 // obtener el valor a cifrar colocado por el usuario
 document.getElementById("botonCifrar1").addEventListener("click", () => {
 
-  let string= document.getElementById("mensajeCifrar").value;
-  let offset=document.getElementById("numeroClave").value;
+  let string = document.getElementById("mensajeCifrar").value;
+  let offset = document.getElementById("numeroClave").value;
 
   //imprimir la palabra cifrada
   try {
-    document.getElementById("root").innerHTML=cipher.encode(string,offset);
+    document.getElementById("root").innerHTML = cipher.encode(string, offset);
   } catch (error) {
     alert(error.message);
   }
@@ -41,16 +40,16 @@ document.getElementById("botonCifrar1").addEventListener("click", () => {
 // obtenetr el valor a descifrar colocado por el usuario
 document.getElementById("botonDescifrar1").addEventListener("click", () => {
 
-  let string= document.getElementById("mensajeCifrar").value;
-  let offset=document.getElementById("numeroClave").value;
+  let string = document.getElementById("mensajeCifrar").value;
+  let offset = document.getElementById("numeroClave").value;
 
- //imprimir la palabra cifrada 
+  //imprimir la palabra cifrada 
   try {
-    document.getElementById("root").innerHTML=cipher.decode(string,offset);
+    document.getElementById("root").innerHTML = cipher.decode(string, offset);
   } catch (error) {
     alert(error.message);
   }
-  
+
 
 
 
