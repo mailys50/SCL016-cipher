6b954207d64fc81fabbe5ed494244641f5d199f3
mailys50 <mailysbrizuela@gmail.com>
2021-02-20 12:00:39 -0300

    correcciones finales

--- src/cipher.js
+++ src/cipher.js
@@ -6,7 +6,7 @@ const cipher = {
     let cifrar = "";
 
 
-    if (string == "" || string == 0 || string == null || string == "[]") {
+    if (string == "" || string == 0 || string == null || string == []) {
 
       throw new TypeError("el caracter introducido no esta permitido");
 
@@ -18,18 +18,18 @@ const cipher = {
     }
 
     else {
-      // convertir valor string a mayuscula, convertir valor de offset a numero
-      string = string.toUpperCase();
-      offset = parseInt(offset);
+        // convertir valor string a mayuscula, convertir valor de offset a numero
+        string = string.toUpperCase();
+        offset = parseInt(offset);
 
-      //obtener el valor de la letra en codigo ASCII, desplazamiento , convertir en letra
-      for (let i = 0; i < string.length; i++) {
+        //obtener el valor de la letra en codigo ASCII, desplazamiento , convertir en letra
+        for (let i = 0; i < string.length; i++) {
 
-        let codigoAscii = string.charCodeAt(i);
-        let recorrido = (codigoAscii - 65 + offset) % 26 + 65;
-        cifrar += String.fromCharCode(recorrido);
+          let codigoAscii = string.charCodeAt(i);
+          let recorrido = (codigoAscii - 65 + offset) % 26 + 65;
+          cifrar += String.fromCharCode(recorrido);
 
-      }
+        }
     }
 
     return cifrar;
@@ -40,31 +40,31 @@ const cipher = {
   decode(string, offset) {
     let descifrar = "";
 
-    if (string == "" || string == 0 || string == null || string == "[]") {
-      //manejo de los errores de typeError
-      throw new TypeError("el caracter introducido no esta permitido");
-
-    }
-    if (offset == "" || offset == 0 || offset == null || offset == "[]") {
-      //manejo de los errores de typeError
-      throw new TypeError("el offset introducido no esta permitido");
-
-    }
-
-    else {
-      // convertir valor de usuario a mayuscula, convertir valor de usuario a numero
-      string = string.toUpperCase();
-      offset = parseInt(offset);
-
-      //obtener el valor de la letra en codigo ASCII, desplazamiento , convertir en letra
-      for (let i = 0; i < string.length; i++) {
-
-        let codigoAscii = string.charCodeAt(i);
-        let recorrido = (codigoAscii + 65 - offset) % 26 + 65;
-        descifrar += String.fromCharCode(recorrido);
+      if (string == "" || string == 0 || string == null || string == []) {
+        //alert("el caracter introducido no esta permitido");
+        throw new TypeError("el caracter introducido no esta permitido");
 
       }
-    }
+      if (offset == "" || offset == 0 || offset == null || offset == []) {
+        //alert("el string introducido no esta permitido");
+        throw new TypeError("el offset introducido no esta permitido");
+
+      }
+
+      else {
+        // convertir valor de usuario a mayuscula, convertir valor de usuario a numero
+        string = string.toUpperCase();
+        offset = parseInt(offset);
+
+        //obtener el valor de la letra en codigo ASCII, desplazamiento , convertir en letra
+        for (let i = 0; i < string.length; i++) {
+
+          let codigoAscii = string.charCodeAt(i);
+          let recorrido = (codigoAscii + 65 - offset) % 26 + 65;
+          descifrar += String.fromCharCode(recorrido);
+
+        }
+      }
 
     return descifrar;
   },
